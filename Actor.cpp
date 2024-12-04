9656d48b6b9d5545500b1b8ef7e398ecb8e3b88a
alfinaeull <150996163+alfinaeull@users.noreply.github.com>
2024-12-03 17:18:17 -0800

    attempted digEarth() but failed

--- Actor.cpp
+++ Actor.cpp
@@ -2,32 +2,41 @@
 #include "StudentWorld.h"
 
 // Students:  Add code to this file (if you wish), Actor.h, StudentWorld.h, and StudentWorld.cpp
+// contains implementations of base, Earth, and Tunnelman classes
 
-Object::Object(int imageID, int startX, int startY, Direction dir, double size, unsigned int depth, StudentWorld* w)
+Object::Object(int imageID, int startX, int startY, Direction dir = right, double size = 1.0, unsigned int depth = 0, StudentWorld* w = nullptr)
 	: GraphObject(imageID, startX, startY, dir, size, depth)
 {
 	setVisible(true);
 	world = w;
 }
 
+// destructor
 Object::~Object()
 {
 
 }
 
+// returns pointer to StudentWorld
 StudentWorld* Object::getWorld()
 {
 	return world;
 }
 
+// virtual method doSomething
+void Object::doSomething()
+{
 
-Earth::Earth(int startX, int startY, Direction dir, double size, unsigned int depth, StudentWorld* sw)
+}
+
+// constructor
+Earth::Earth(int startX, int startY, Direction dir = right, double size = .25, unsigned int depth = 3, StudentWorld* sw = nullptr)
 	: Object(TID_EARTH, startX, startY, dir, size, depth, sw)
 {
 
 }
 
-
+// destructor
 Earth::~Earth()
 {
 
@@ -38,18 +47,21 @@ void Earth::doSomething()
 
 }
 
-Tunnelman::Tunnelman(int startX, int startY, Direction dir, double size, unsigned int depth, StudentWorld* sw)
+// constructor
+Tunnelman::Tunnelman(int startX = 30, int startY = 60, Direction dir = right, double size = 1.0, unsigned int depth = 0, StudentWorld* sw = nullptr)
 	: Object(TID_PLAYER, startX, startY, dir, size, depth, sw)
 {
 	currX = startX;
 	currY = startY;
 }
 
+// destructor
 Tunnelman::~Tunnelman()
 {
 
 }
 
+// limited doSomething method
 void Tunnelman::doSomething()
 {
 	int key;
@@ -96,15 +108,14 @@ void Tunnelman::doSomething()
 	// remove any Earth objects that overlap with Tunnelman's 4x4 image
 	for (int i = currX; i < currX + 4; i++) {
 
-				for (int j = currY; j < currY + 4; j++) {
+		for (int j = currY; j < currY + 4; j++) {
 
-					if (i > 0 && i < 64 && j > 0 && j < 60) {
+			if (i > 0 && i < 64 && j > 0 && j < 60) {
 
-						getWorld()->digEarth(i, j);
-					}
-
-				}
+				getWorld()->digEarth(i, j);
 			}
 
+		}
+	}
 
 }
